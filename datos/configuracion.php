a029f4919cec73f721aaae45bc58b962d3a3f671
cisnerosmaycol <40906417+cisnerosmaycol@users.noreply.github.com>
2018-09-10 21:21:31 -0500

    Add files via upload

---
+++ datos/configuracion.php
@@ -0,0 +1,12 @@
+<?php
+
+// Notificar todos los errores excepto E_NOTICE
+error_reporting(0);
+
+define("SERVIDOR_BD", "ec2-54-163-246-5.compute-1.amazonaws.com");
+define("USUARIO_BD", "zusmittntytbve");
+define("CLAVE_BD", "72f212700b2953bec4124a9503921b85f18579fd59238f30e576b8d37ba23243");
+define("PUERTO_BD", "5432");
+define("NOMBRE_BD", "d6q6fa84l4udst");
+
+ 
